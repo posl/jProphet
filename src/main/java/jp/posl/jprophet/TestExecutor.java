55435a0518b7eb57314d2c4ea4f5f1b0cf10db9a
Asada0815 <asada_mac@poslnoMacBook-puro.local>
2019-06-27 18:41:43 +0900

    first

--- src/main/java/jp/posl/jprophet/TestExecutor.java
+++ src/main/java/jp/posl/jprophet/TestExecutor.java
@@ -1,14 +1,13 @@
 package jp.posl.jprophet;
 
-import jp.posl.jprophet.ProjectConfiguration;
-
-
 public class TestExecutor {
 	public TestExecutor() {
-		
+
 	}
-	
+
 	public boolean test(ProjectConfiguration projectConfiguration) {
+
+
 		return true;
 	}
 }
