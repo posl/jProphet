d019a32a2441b54378416532e93d62c367fa4148
Asada0815 <asada_mac@poslnoMacBook-puro.local>
2019-07-09 18:12:56 +0900

    Created program which run python from java

--- src/main/java/jp/posl/jprophet/JProphetMain.java
+++ src/main/java/jp/posl/jprophet/JProphetMain.java
@@ -1,46 +1,45 @@
 package jp.posl.jprophet;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.HashMap;
-import jp.posl.jprophet.ProjectConfiguration;
-import jp.posl.jprophet.FaultLocalization;
-import jp.posl.jprophet.AstNode;
-import jp.posl.jprophet.AstGenerator;
-import jp.posl.jprophet.AbstractRepairCandidate;
-import jp.posl.jprophet.ConcreteRepairCandidate;
-import jp.posl.jprophet.RepairCandidateGenerator;
-import jp.posl.jprophet.PlausibilityAnalyzer;
-import jp.posl.jprophet.StagedCondGenerator;
+import java.util.List;
 
 
-public class JProphetMain {   
-	
-	
-	
+public class JProphetMain {
+
+
+
     public static void main(String[] args) {
+
+
+
     	ProjectConfiguration project = new ProjectConfiguration();
-    	
+
+
+    	TestExecutor  t = new TestExecutor();
+    	t.test(project);
+
+
     	// フォルトローカライゼーション
     	FaultLocalization faultLocalization = new FaultLocalization();
     	HashMap<AstNode, Integer> suspiciousenesses = faultLocalization.exec(project);
-    	
+
     	// 修正対象コードの全ASTノードの取得
     	AstGenerator astGenerator = new AstGenerator();
     	List<AstNode> astNodes = astGenerator.getAllASTNode();
-    	
+
     	// 各ASTに対して修正テンプレートを適用し抽象修正候補の生成
     	RepairCandidateGenerator repairCandidateGenerator = new RepairCandidateGenerator();
     	List<AbstractRepairCandidate> abstractRepairCandidates = new ArrayList<AbstractRepairCandidate>();
     	for(AstNode astNode : astNodes) {
     		abstractRepairCandidates.addAll(repairCandidateGenerator.applyTemplate(astNode));
     	}
-    	
+
     	// 学習モデルとフォルトローカライゼーションのスコアによってソート
-    	PlausibilityAnalyzer plausibilityAnalyzer = new PlausibilityAnalyzer();  
+    	PlausibilityAnalyzer plausibilityAnalyzer = new PlausibilityAnalyzer();
     	List<AbstractRepairCandidate> sortedAbstractRepairCandidate = plausibilityAnalyzer.sortRepairCandidates(abstractRepairCandidates, suspiciousenesses);
-    	
-    	
+
+
     	// 抽象修正候補中の条件式の生成
     	StagedCondGenerator stagedCondGenerator = new StagedCondGenerator();
     	TestExecutor        testExecutor        = new TestExecutor();
@@ -54,7 +53,7 @@ public class JProphetMain {
     			}
     		}
     	}
-    	
-    	
+
+
     }
 }
--- src/main/java/jp/posl/jprophet/TestExecutor.java
+++ src/main/java/jp/posl/jprophet/TestExecutor.java
@@ -1,5 +1,11 @@
 package jp.posl.jprophet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class TestExecutor {
 	public TestExecutor() {
 
@@ -8,6 +14,29 @@ public class TestExecutor {
 	public boolean test(ProjectConfiguration projectConfiguration) {
 
 
-		return true;
+        Runtime runtime = Runtime.getRuntime();
+
+        String[] Command = { "python", "./test.py" };
+
+        Process p = null;
+        File dir = new File("./");
+
+
+        try {
+        	p = runtime.exec(Command, null, dir);
+            p.waitFor();
+            BufferedReader br =
+              new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+               System.out.print(line);
+            }
+          } catch (IOException e) {
+             System.out.print(e);
+          } catch (InterruptedException e) {
+             System.out.print(e);
+          }
+
+        return true;
 	}
 }
