776aa26f469642dd10a239ddf4b6f1ec103efd37
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-08-15 05:06:40 +0900

    Fix function in getAllAstNode()

--- src/main/java/jp/posl/jprophet/AstGenerator.java
+++ src/main/java/jp/posl/jprophet/AstGenerator.java
@@ -34,6 +34,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -56,26 +57,32 @@ public class AstGenerator {
 		List<Node> nodes = new ArrayList<Node>(parentNode.getChildNodes());
 		int index = 0;
 		while(true){
+			if(index == nodes.size()) break;
 			Node node = nodes.get(index);
 			
 			if(!node.getChildNodes().isEmpty()){
 				nodes.addAll(node.getChildNodes());
 			}
 			index++;
-			if(index == nodes.size()) break;
 		}
 		return nodes;
 	}
 
+
 	// コンパイルユニットから与えられたハッシュ値を持つノードを見つける
-	private Optional<Node> findNode(CompilationUnit cu, int hashCode){
+	private Optional<Node> findNode(CompilationUnit cu, int hashCode, HashMap<Integer, Integer> hashCodeCounts){
 		List<Node> nodes = new ArrayList<Node>();
 		nodes.add(cu.findRootNode());
 		int index = 0;
+		int count = 0;
 		while(true){
 			Node node = nodes.get(index);
 			if(node.hashCode() == hashCode) {
-				return Optional.ofNullable(node);
+				count++;
+				if(count == hashCodeCounts.get(hashCode)){
+					hashCodeCounts.put(node.hashCode(), hashCodeCounts.get(hashCode) - 1);
+					return Optional.ofNullable(node);
+				}
 			}
 			else if(!node.getChildNodes().isEmpty()){
 				nodes.addAll(node.getChildNodes());
@@ -86,10 +93,31 @@ public class AstGenerator {
 		return Optional.ofNullable(null);
 	}
 
-	// 全てAstNodeを取得
+
+	// 同一のハッシュ値を持つノードを数え上げる
+	private HashMap<Integer, Integer> countHashCode(List<Node> nodes){
+		List<Integer> codes = new ArrayList<Integer>();
+		HashMap<Integer, Integer> hashCodeCounts = new HashMap<Integer, Integer>();
+		for(Node node: nodes){
+			codes.add(node.hashCode());
+			if(hashCodeCounts.containsKey(node.hashCode())){
+				int count = hashCodeCounts.get(node.hashCode());
+				hashCodeCounts.put(node.hashCode(), ++count);
+			}
+			else{
+				hashCodeCounts.put(node.hashCode(), 1);
+			}
+		}
+		return hashCodeCounts;
+	}
+
+
+	// 全てのAstNodeを取得
 	public List<AstNode> getAllAstNode(){
 		Node root = this.cu.findRootNode();
 		List<Node> nodes = this.collectAllChildNodes(root);
+
+		HashMap<Integer, Integer> hashCodeCounts = countHashCode(nodes);
 
 		List<AstNode> astNodes = new ArrayList<AstNode>();
 		for(Node node : nodes){
@@ -97,7 +125,7 @@ public class AstGenerator {
 			try{
 				cUnit = JavaParser.parse(new File(this.FILE_PATH));
 				LexicalPreservingPrinter.setup(cUnit);
-				this.findNode(cUnit, node.hashCode()).ifPresent(n -> {
+				this.findNode(cUnit, node.hashCode(), hashCodeCounts).ifPresent(n -> {
 					astNodes.add(new AstNode(n, cUnit));
 				});
 			}
