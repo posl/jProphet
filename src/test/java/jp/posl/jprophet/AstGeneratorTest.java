f0c49c78ce68e5911ee60588c3d786e86ada4ed3
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-08-20 11:09:17 +0900

    Add test for compilation unit generation in AstGenerator

--- src/test/java/jp/posl/jprophet/AstGeneratorTest.java
+++ src/test/java/jp/posl/jprophet/AstGeneratorTest.java
@@ -5,22 +5,61 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 
-public class AstGeneratorTest {
-    private final String source = new StringBuilder().append("")
-        .append("public class A {\n")
-        .append("   public void a() {\n")
-        .append("   }\n")
-        .append("}\n")
-        .toString();
-    private List<RepairUnit> candidates = new AstGenerator().getAllRepairUnit(source);
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.VariableDeclarator;
 
-    @Test public void testForGeneratedAst() {
-        assertThat(this.candidates.size()).isEqualTo(6);
-        assertThat(this.candidates.get(0).getNode().toString()).isEqualTo("public class A {\n\n    public void a() {\n    }\n}");
-        assertThat(this.candidates.get(1).getNode().toString()).isEqualTo("A");
-        assertThat(this.candidates.get(2).getNode().toString()).isEqualTo("public void a() {\n}");
-        assertThat(this.candidates.get(3).getNode().toString()).isEqualTo("a");
-        assertThat(this.candidates.get(4).getNode().toString()).isEqualTo("void");
-        assertThat(this.candidates.get(5).getNode().toString()).isEqualTo("{\n}");
+public class AstGeneratorTest {
+
+    @Test public void testForGeneratedAstString() {
+        final String source = new StringBuilder().append("")
+            .append("public class A {\n")
+            .append("   public void a() {\n")
+            .append("   }\n")
+            .append("}\n")
+            .toString();
+        List<RepairUnit> repairUnit = new AstGenerator().getAllRepairUnit(source);
+        
+        // ASTが取得できているか確認
+        assertThat(repairUnit.size()).isEqualTo(6);
+        assertThat(repairUnit.get(0).getNode().toString()).isEqualTo("public class A {\n\n    public void a() {\n    }\n}");
+        assertThat(repairUnit.get(1).getNode().toString()).isEqualTo("A");
+        assertThat(repairUnit.get(2).getNode().toString()).isEqualTo("public void a() {\n}");
+        assertThat(repairUnit.get(3).getNode().toString()).isEqualTo("a");
+        assertThat(repairUnit.get(4).getNode().toString()).isEqualTo("void");
+        assertThat(repairUnit.get(5).getNode().toString()).isEqualTo("{\n}");
     }   
+
+    @Test public void testForCompilationUnit(){
+        final String source = new StringBuilder().append("")
+            .append("public class A {\n")
+            .append("   public void a() {\n")
+            .append("       int i = 0;\n")
+            .append("   }\n")
+            .append("}\n")
+            .toString();
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(source);
+        assertThat(repairUnits.size()).isEqualTo(12);
+        RepairUnit targetUnit = repairUnits.get(8);
+        
+        // それぞれのRepairUnitの持つコンパイルユニットが別インスタンスで，かつtargetNodeのコンパイルユニットと
+        // 一致していることの確認 
+        for(RepairUnit repairUnit : repairUnits){
+            assertThat(repairUnit.getNode().findRootNode()).isSameAs(repairUnit.getCompilationUnit());
+        }
+        for(int i = 0; i < repairUnits.size(); i++){
+            if(i == 8) continue;
+            assertThat(targetUnit.getCompilationUnit()).isNotSameAs(repairUnits.get(i).getCompilationUnit());
+        }
+
+        Node node = targetUnit.getNode();
+        assertThat(node).isInstanceOf(VariableDeclarator.class);
+        ((VariableDeclarator)node).setName("test");
+        assertThat(targetUnit.getCompilationUnit().toString()).contains("test");       
+
+        // 実際にASTに変更を加えて確認(念の為)
+        for(int i = 0; i < repairUnits.size(); i++){
+            if(i == 8) continue;
+            assertThat(repairUnits.get(i).getCompilationUnit().toString()).doesNotContain("test");       
+        }
+    }
 }
