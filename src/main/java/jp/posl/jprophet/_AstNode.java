0113183b4cf8efc630b45709b9256e13273fcec2
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-08-20 18:36:53 +0900

    Add empty test for RepairCandidateGenerator

--- src/main/java/jp/posl/jprophet/_AstNode.java
+++
@@ -1,8 +0,0 @@
-// package jp.posl.jprophet;
-
-
-// public interface AstNode {
-//     public String getSourceCode();
-//     public void accept(RepairCandidateGenerator rcGenerator);
-//     public int getLineNumber();
-// }
---
+++ src/test/java/jp/posl/jprophet/RepairCandidateGeneratorTest.java
@@ -0,0 +1,10 @@
+package jp.posl.jprophet;
+
+import org.junit.Test;                                                                                                                                                                  
+import static org.assertj.core.api.Assertions.*;
+
+public class RepairCandidateGeneratorTest {
+    @Test public void test(){
+        
+    }
+}
