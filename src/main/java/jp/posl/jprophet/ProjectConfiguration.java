247980a45063b2feb38bd200ce39b9be4a10b91b
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-08-26 18:12:54 +0900

    Add ProjectBuilder

--- src/main/java/jp/posl/jprophet/ProjectConfiguration.java
+++ src/main/java/jp/posl/jprophet/ProjectConfiguration.java
@@ -1,18 +1,30 @@
 package jp.posl.jprophet;
 
-import java.util.ArrayList;
 import java.util.List;
 
+
 public class ProjectConfiguration {
-	List<String> filePaths = new ArrayList<String>();
-	public ProjectConfiguration(String[] args) {
-		if(args.length < 1)
-			this.filePaths.add("example/target01.java");
-		else
-			this.filePaths.add(args[0]);
+	private final List<String> sourceFilePaths;
+	private final List<String> testFilePaths;
+	private final List<String> classPaths;
+
+	public ProjectConfiguration(final List<String> sourceFilePaths, final List<String> testFilePaths, final List <String> classPaths) {
+		this.sourceFilePaths = sourceFilePaths;
+		this.testFilePaths = testFilePaths;
+		this.classPaths = classPaths;
+		
+		if(this.sourceFilePaths.size() < 1)
+			this.sourceFilePaths.add("example/target01.java");
+	}
+	public List<String> getSourceFilePaths() {
+		return this.sourceFilePaths;
 	}
 
-	public List<String> getFilePaths(){
-		return this.filePaths;
+	public List<String> getTestFilePaths() {
+		return testFilePaths;
+	}
+
+	public List<String> getClassPaths() {
+		return classPaths;
 	}
 }
