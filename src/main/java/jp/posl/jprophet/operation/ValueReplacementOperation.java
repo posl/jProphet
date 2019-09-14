ea613ce27877e82aea77de1590615a1361ff29b4
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-09-14 15:36:15 +0900

    Add method for collect variables

--- src/main/java/jp/posl/jprophet/operation/ValueReplacementOperation.java
+++ src/main/java/jp/posl/jprophet/operation/ValueReplacementOperation.java
@@ -3,14 +3,70 @@ package jp.posl.jprophet.operation;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.AssignExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+
 import jp.posl.jprophet.RepairUnit;
 
 /**
  * 対象ステートメントの変数や関数，定数などを別のもので置き換える操作
  */
-public class ValueReplacementOperation implements AstOperation{
-    public List<RepairUnit> exec(RepairUnit repairUnit){
+public class ValueReplacementOperation implements AstOperation {
+    public List<RepairUnit> exec(RepairUnit repairUnit) {
+        Node targetNode = repairUnit.getNode();
+
+        if (targetNode instanceof AssignExpr) {
+            // ((AssignExpr) targetNode).setValue(new FieldAccessExpr(new ThisExpr(),
+            // "hoge"));
+            // ((AssignExpr) targetNode).setValue(new NameExpr("lc"));
+            Node classNode = targetNode.findParent(ClassOrInterfaceDeclaration.class).get();
+            Node methodNode =  targetNode.findParent(MethodDeclaration.class).get();
+            List<FieldDeclaration> fields = this.collectChild(classNode, FieldDeclaration.class);
+            List<VariableDeclarationExpr> localVariables = this.collectChild(methodNode, VariableDeclarationExpr.class);
+        }
+
         List<RepairUnit> candidates = new ArrayList<RepairUnit>();
         return candidates;
     }
+
+    private <T extends Node> List<T> collectChild(Node scope, Class<T> nodeType){
+        List<Node> nodes = this.collectAllChildNodes(scope);
+        List<T> fields = new ArrayList<T>();
+        for(Node node : nodes){
+            if(nodeType.isAssignableFrom(node.getClass())){
+                fields.add(nodeType.cast(node));
+            }
+        }
+
+        return fields;
+    }
+
+
+	/**
+	 * ASTノードの全ての子ノードをリストにして返す.
+	 * 
+	 * @param parentNode 探索したい子ノードを持つASTノード
+	 * @return 子ノードのリスト 
+	 */
+	private List<Node> collectAllChildNodes(Node parentNode){
+		List<Node> childNodes = new ArrayList<Node>(parentNode.getChildNodes());
+		int index = 0;
+		while(true){
+			if(index == childNodes.size()) break;
+			Node node = childNodes.get(index);
+			
+			if(!node.getChildNodes().isEmpty()){
+				childNodes.addAll(node.getChildNodes());
+			}
+			index++;
+		}
+		return childNodes;
+	}
+    
+
+
 }
---
+++ src/test/java/jp/posl/jprophet/operation/ValueReplacementOperationTest.java
@@ -0,0 +1,39 @@
+package jp.posl.jprophet.operation;
+
+import org.junit.Test;
+
+import jp.posl.jprophet.AstGenerator;
+import jp.posl.jprophet.RepairUnit;
+import static org.assertj.core.api.Assertions.*;
+import java.util.List;
+
+public class ValueReplacementOperationTest{
+    private final String sourceMethod = new StringBuilder().append("")
+    .append("public class A {\n")
+    .append("   private void ma() {\n")
+    .append("       this.mb();\n")
+    .append("   }\n")
+    .append("   private void mb() {\n")
+    .append("   }\n")
+    .append("   private void mc() {\n")
+    .append("   }\n")
+    .append("}\n")
+    .toString();
+
+    private final String sourceValue = new StringBuilder().append("")
+    .append("public class A {\n")
+    .append("   private String ma = \"a\";\n")
+    .append("   private String mb = \"b\";\n")
+    .append("   private void ma() {\n")
+    .append("       String lb = \"b\";\n")
+    .append("       String lc = \"c\";\n")
+    .append("       this.ma = lb;\n")
+    .append("   }\n")
+    .append("}\n")
+    .toString();
+    @Test public void test(){
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(this.sourceValue);
+        ValueReplacementOperation op = new ValueReplacementOperation();
+        op.exec(repairUnits.get(23));
+    }
+}
