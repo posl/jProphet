9d947923308ef7c76acc38fdfea4804240fed7c6
Yamate Kyosuke <yamate@poslnoMacBook-Pro.local>
2019-09-16 09:58:22 +0900

    get FQDN

--- src/main/java/jp/posl/jprophet/FaultLocalization.java
+++ src/main/java/jp/posl/jprophet/FaultLocalization.java
@@ -13,19 +13,49 @@ import java.nio.file.Paths;
 import java.io.File;
 import java.util.stream.Collectors;
 import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 
 
 public class FaultLocalization {
 	ProjectBuilder projectBuilder = new ProjectBuilder();
 	Path classDir;
+	String buildPath;
 	List<String> classFilePaths;
+	List<String> SourceClassFilePaths = new ArrayList<String>();
+	List<String> TestClassFilePaths = new ArrayList<String>();
 
 	public FaultLocalization(ProjectConfiguration project) {
 		this.projectBuilder.build(project);
+		this.buildPath = project.getBuildPath();
+		//System.out.println(project.getSourceFilePaths());
+		//System.out.println(project.getTestFilePaths());
+
+		List<String> SourceFilePaths = project.getSourceFilePaths();
+		List<String> TestFilePaths = project.getTestFilePaths();
+		List<String> SourceFileNames = new ArrayList<String>();
+		List<String> TestFileNames = new ArrayList<String>();
+
+		int Ssize = project.getSourceFilePaths().size();
+		for(int i = 0; i < Ssize; i++){	
+			String str = SourceFilePaths.get(i);
+			File file = new File(str);
+			SourceFileNames.add(file.getName().replace(".java", ".class"));
+		}
+
+		int Tsize = project.getTestFilePaths().size();
+		for(int i = 0; i < Tsize; i++){	
+			String str = TestFilePaths.get(i);
+			File file = new File(str);
+			TestFileNames.add(file.getName().replace(".java", ".class"));
+		}
+
+		//System.out.println(SourceFileNames);
+		//System.out.println(TestFileNames);
 
 		try{
-		this.classDir =  Paths.get("FLtmp");
+		this.classDir =  Paths.get(this.buildPath);
 
 		List<File> classFilelist = Files.walk(classDir).map(path -> path.toFile()).filter(file -> file.isFile()).collect(Collectors.toList());
 
@@ -37,7 +67,27 @@ public class FaultLocalization {
 			//例外処理
 		}
 			
-		System.out.println(classFilePaths);
+		//System.out.println(classFilePaths);
+
+		int Csize = classFilePaths.size();
+		for(int i = 0; i < Csize; i++){	
+			String str = classFilePaths.get(i);
+			for(int j = 0; j < Ssize; j++){	
+				String Sstr = SourceFileNames.get(j);
+				if (str.contains(Sstr)){
+					SourceClassFilePaths.add(str.replace(buildPath, "").replace(".class", "").replace("/", ".").substring(1));
+				}
+			}
+			for(int k = 0; k < Tsize; k++){	
+				String Tstr = TestFileNames.get(k);
+				if (str.contains(Tstr)){
+					TestClassFilePaths.add(str.replace(buildPath, "").replace(".class", "").replace("/", ".").substring(1));
+				}
+			}
+		}
+
+		System.out.println("Source FQDN -> " + SourceClassFilePaths);
+		System.out.println("Test FQDN -> " + TestClassFilePaths);
 
 	}
 
--- src/test/java/jp/posl/jprophet/FaultLocalizationTest.java
+++ src/test/java/jp/posl/jprophet/FaultLocalizationTest.java
@@ -11,7 +11,7 @@ import jp.posl.jprophet.FaultLocalization;
 public class FaultLocalizationTest{
     // 入力として用意するテスト用のプロジェクト
     private String projectPath = "src/test/resources/testGradleProject01";
-    private ProjectConfiguration project = new ProjectConfiguration(this.projectPath, "FLtmp");
+    private ProjectConfiguration project = new ProjectConfiguration(this.projectPath, "Ftmp");
 
     /**
      * ソースファイルを正しく取得できているかの検証
