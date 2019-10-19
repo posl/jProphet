f992291270cf5b299c35dadafa6977ccdf161a24
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-10-19 15:55:25 +0900

    Add test code

--- src/test/java/jp/posl/jprophet/operation/VariableReplacementOperationTest.java
+++ src/test/java/jp/posl/jprophet/operation/VariableReplacementOperationTest.java
@@ -5,20 +5,12 @@ import org.junit.Test;
 import jp.posl.jprophet.AstGenerator;
 import jp.posl.jprophet.RepairUnit;
 import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class VariableReplacementOperationTest{
-    private final String sourceForFieldReplacement = new StringBuilder().append("")
-    .append("public class A {\n")
-    .append("   private String fa = \"a\";\n")
-    .append("   private String fb = \"b\";\n")
-    .append("   private void ma() {\n")
-    .append("       this.fa = 'hoge';\n")
-    .append("   }\n")
-    .append("}\n")
-    .toString();
-
-    private final String sourceForLocalVarReplacement = new StringBuilder().append("")
+    private final String sourceForAssignmentReplace = new StringBuilder().append("")
     .append("public class A {\n")
     .append("   private String fa = \"a\";\n")
     .append("   private void ma() {\n")
@@ -29,35 +21,56 @@ public class VariableReplacementOperationTest{
     .append("}\n")
     .toString();
 
-    private final String sourceForArgumentReplacement = new StringBuilder().append("")
+    private final String sourceForArgumentReplace = new StringBuilder().append("")
     .append("public class A {\n")
     .append("   private String fa = \"a\";\n")
     .append("   private String fb = \"a\";\n")
+    .append("   private String fc = \"a\";\n")
     .append("   private void ma() {\n")
     .append("       String la = \"b\";\n")
-    .append("       this.mb(a, b);\n")
+    .append("       this.mb(fa, fb);\n")
+    .append("       this.mb(fa);\n")
     .append("   }\n")
     .append("   private void mb(String a) {\n")
     .append("   }\n")
     .append("}\n")
     .toString();
 
-    private final String source = new StringBuilder().append("")
+    private final String sourceThatHasNothingToReplace = new StringBuilder().append("")
+    .append("import java.util.List;\n")
     .append("public class A {\n")
-    .append("   private String ma = \"a\";\n")
-    .append("   private String mb = \"b\";\n")
-    .append("   private void ma() {\n")
-    .append("       String lb = \"b\";\n")
-    .append("       String lc = \"c\";\n")
-    .append("       this.ma = lb;\n")
-    .append("   }\n")
     .append("}\n")
     .toString();
 
-    @Test public void test(){
-        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(this.sourceForArgumentReplacement);
-        VariableReplacementOperation op = new VariableReplacementOperation(repairUnits.get(28));
-        List<RepairUnit> candidates = op.exec();
+
+    @Test public void testForArgumentReplace(){
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(this.sourceForArgumentReplace);
+        List<RepairUnit> candidates = new ArrayList<RepairUnit>();
+        for(RepairUnit repairUnit : repairUnits){
+            VariableReplacementOperation vr = new VariableReplacementOperation(repairUnit);
+            candidates.addAll(vr.exec());
+        }
         return;
     }
+
+    @Test public void testForAssignmentReplace(){
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(this.sourceForAssignmentReplace);
+        List<RepairUnit> candidates = new ArrayList<RepairUnit>();
+        for(RepairUnit repairUnit : repairUnits){
+            VariableReplacementOperation vr = new VariableReplacementOperation(repairUnit);
+            candidates.addAll(vr.exec());
+        }
+        return;
+    }
+
+    @Test public void testForWhenThereIsNoReplacement(){
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(this.sourceThatHasNothingToReplace);
+        List<RepairUnit> candidates = new ArrayList<RepairUnit>();
+        for(RepairUnit repairUnit : repairUnits){
+            VariableReplacementOperation vr = new VariableReplacementOperation(repairUnit);
+            candidates.addAll(vr.exec());
+        }
+        return;
+    }
+
 }
