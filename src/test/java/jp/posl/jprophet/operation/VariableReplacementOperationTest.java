6620b3799840e1c43ac33949da51b0def308941c
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-10-23 00:06:02 +0900

    Add test method for Variable Replacement Operation

--- src/test/java/jp/posl/jprophet/operation/VariableReplacementOperationTest.java
+++ src/test/java/jp/posl/jprophet/operation/VariableReplacementOperationTest.java
@@ -8,69 +8,135 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class VariableReplacementOperationTest{
-    private final String sourceForAssignmentReplace = new StringBuilder().append("")
-    .append("public class A {\n")
-    .append("   private String fa = \"a\";\n")
-    .append("   private void ma() {\n")
-    .append("       String la = \"b\";\n")
-    .append("       String lb = \"c\";\n")
-    .append("       this.fa = lb;\n")
-    .append("   }\n")
-    .append("}\n")
-    .toString();
+    
 
-    private final String sourceForArgumentReplace = new StringBuilder().append("")
-    .append("public class A {\n")
-    .append("   private String fa = \"a\";\n")
-    .append("   private String fb = \"a\";\n")
-    .append("   private String fc = \"a\";\n")
-    .append("   private void ma() {\n")
-    .append("       String la = \"b\";\n")
-    .append("       this.mb(fa, fb);\n")
-    .append("       this.mb(fa);\n")
-    .append("   }\n")
-    .append("   private void mb(String a) {\n")
-    .append("   }\n")
-    .append("}\n")
-    .toString();
-
-    private final String sourceThatHasNothingToReplace = new StringBuilder().append("")
-    .append("import java.util.List;\n")
-    .append("public class A {\n")
-    .append("}\n")
-    .toString();
-
-
+    /**
+     * 引数を変数に置換する機能のテスト
+     */
     @Test public void testForArgumentReplace(){
-        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(this.sourceForArgumentReplace);
-        List<RepairUnit> candidates = new ArrayList<RepairUnit>();
+        final String targetStatement = 
+                "       this.mb(\"hoge\", \"fuga\");\n";
+
+        final String source = new StringBuilder().append("")
+        .append("public class A {\n")
+        .append("   private String fa = \"a\";\n")
+        .append("   private void ma() {\n")
+        .append("       String la = \"b\";\n")
+        .append(targetStatement)
+        .append("   }\n")
+        .append("   private void mb(String a) {\n")
+        .append("   }\n")
+        .append("}\n")
+        .toString();
+
+        List<String> expectedTargetSources = new ArrayList<String>();
+        expectedTargetSources.add("this.mb(this.fa, \"fuga\")");
+        expectedTargetSources.add("this.mb(\"hoge\", this.fa)");
+        expectedTargetSources.add("this.mb(la, \"fuga\")");
+        expectedTargetSources.add("this.mb(\"hoge\", la)");
+
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(source);
+        List<String> candidateSources = new ArrayList<String>();
         for(RepairUnit repairUnit : repairUnits){
             VariableReplacementOperation vr = new VariableReplacementOperation(repairUnit);
-            candidates.addAll(vr.exec());
+            candidateSources.addAll(vr.exec().stream()
+                .map(ru -> ru.toString())
+                .collect(Collectors.toList())
+            );
         }
+        assertThat(candidateSources).containsOnlyElementsOf(expectedTargetSources);
         return;
     }
 
+    /**
+     * 代入文の左辺をプログラム中の変数で置換できるかテスト 
+     */
     @Test public void testForAssignmentReplace(){
-        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(this.sourceForAssignmentReplace);
-        List<RepairUnit> candidates = new ArrayList<RepairUnit>();
+        final String targetStatement = 
+                "       la = \"hoge\";\n"; 
+
+        final String source = new StringBuilder().append("")
+        .append("public class A {\n")
+        .append("   private String fa = \"a\";\n")
+        .append("   private void ma() {\n")
+        .append("       String la = \"a\";\n")
+        .append("       String lb = \"b\";\n")
+        .append(targetStatement)
+        .append("   }\n")
+        .append("}\n")
+        .toString();
+
+        List<String> expectedTargetSources = new ArrayList<String>();
+        expectedTargetSources.add("la = la");
+        expectedTargetSources.add("la = lb");
+        expectedTargetSources.add("la = this.fa");
+
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(source);
+        List<String> candidateSources = new ArrayList<String>();
         for(RepairUnit repairUnit : repairUnits){
             VariableReplacementOperation vr = new VariableReplacementOperation(repairUnit);
-            candidates.addAll(vr.exec());
+            candidateSources.addAll(vr.exec().stream()
+                .map(ru -> ru.toString())
+                .collect(Collectors.toList())
+            );
         }
+
+        assertThat(candidateSources).containsOnlyElementsOf(expectedTargetSources);
         return;
     }
 
+    /**
+     * クラス外のステートメントに対して正常に動作するかテスト
+     */
     @Test public void testForWhenThereIsNoReplacement(){
-        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(this.sourceThatHasNothingToReplace);
+        final String sourceThatHasNothingToReplace = new StringBuilder().append("")
+        .append("import java.util.List;\n")
+        .toString();
+
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(sourceThatHasNothingToReplace);
         List<RepairUnit> candidates = new ArrayList<RepairUnit>();
         for(RepairUnit repairUnit : repairUnits){
             VariableReplacementOperation vr = new VariableReplacementOperation(repairUnit);
             candidates.addAll(vr.exec());
         }
+
+        assertThat(candidates.size()).isZero();
         return;
     }
 
+    /**
+     * 生成した修正パッチ候補に元のステートメントと同じものが含まれていないことをテスト
+     */
+    @Test public void testThatCandidatesDoesNotContainOriginal(){
+        final String targetStatement = 
+                "       la = lb;\n"; 
+
+        final String source = new StringBuilder().append("")
+        .append("public class A {\n")
+        .append("   private void ma() {\n")
+        .append("       String la = \"a\";\n")
+        .append("       String lb = \"b\";\n")
+        .append(targetStatement)
+        .append("   }\n")
+        .append("}\n")
+        .toString();
+
+        final String targetStatementAsRepairUnitToString = "la = lb"; 
+
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(source);
+        List<String> candidateSources = new ArrayList<String>();
+        for(RepairUnit repairUnit : repairUnits){
+            VariableReplacementOperation vr = new VariableReplacementOperation(repairUnit);
+            candidateSources.addAll(vr.exec().stream()
+                .map(ru -> ru.toString())
+                .collect(Collectors.toList())
+            );
+        }
+
+        assertThat(candidateSources).doesNotContain(targetStatementAsRepairUnitToString);
+        return;
+    }
 }
