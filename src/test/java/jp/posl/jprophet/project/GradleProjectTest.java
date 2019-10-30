ab4c395e452f1783643b94ec0e33e65838897050
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-10-30 23:13:27 +0900

    update test

--- src/test/java/jp/posl/jprophet/project/GradleProjectTest.java
+++ src/test/java/jp/posl/jprophet/project/GradleProjectTest.java
@@ -48,9 +48,9 @@ public class GradleProjectTest{
          
         List<FileLocator> actualFileLocators = this.project.getTestFileLocators();
         // assertThat(actualFileLocators.get(0).getPath()).contains(expectedTestFilePaths.get(1));
-        // assertThat(actualFileLocators.get(1).getPath()).contains(expectedTestFilePaths.get(0));
-        // assertThat(actualFileLocators.get(0).getFqn()).contains(expectedTestFileFqns.get(1));
-        // assertThat(actualFileLocators.get(1).getFqn()).contains(expectedTestFileFqns.get(0));
+        assertThat(actualFileLocators.get(1).getPath()).contains(expectedTestFilePaths.get(0));
+        assertThat(actualFileLocators.get(0).getFqn()).contains(expectedTestFileFqns.get(1));
+        assertThat(actualFileLocators.get(1).getFqn()).contains(expectedTestFileFqns.get(0));
     }
 
     /**
