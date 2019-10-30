b9c567bff62ebb2b9390ca4c83a02cd5f59b57d4
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-10-30 18:47:05 +0900

    Add test for default patch candidate

--- src/test/java/jp/posl/jprophet/DefaultPatchCandidateTest.java
+++ src/test/java/jp/posl/jprophet/DefaultPatchCandidateTest.java
@@ -9,18 +9,18 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class DefaultPatchCandidateTest {
-    /**
-     * getLineNumberのテスト
-     * src/test/resources/test01.java のメソッド定義の先頭の行番号をテスト
-     */
-    @Test public void testForGetLineNumber() {
-        String filePath = "src/test/resources/test01.java";
-        CompilationUnit compilationUnit;
+    private PatchCandidate patchCandidate;
+    private String filePath = "src/test/resources/test01.java";
+    private CompilationUnit compilationUnit;
+    private String fqn = "test01";
+
+    @Before public void setUp() {
         try {
-            compilationUnit = JavaParser.parse(Paths.get(filePath));
+            this.compilationUnit = JavaParser.parse(Paths.get(this.filePath));
         }
         catch (IOException e){
             e.printStackTrace();
@@ -29,9 +29,43 @@ public class DefaultPatchCandidateTest {
         }
         Node targetNode = compilationUnit.findRootNode().getChildNodes().get(0).getChildNodes().get(2);
         RepairUnit repairUnit = new RepairUnit(targetNode, 3, compilationUnit);
-        PatchCandidate patchCandidate = new DefaultPatchCandidate(repairUnit, filePath, "test01");
+        this.patchCandidate = new DefaultPatchCandidate(repairUnit, filePath, fqn);
+    }
 
-        int actualLineNumber = patchCandidate.getLineNumber().get();
+    /**
+     * getLineNumberのテスト
+     * src/test/resources/test01.java のメソッド定義の先頭の行番号をテスト
+     */
+    @Test public void testForGetLineNumber() {
+        int actualLineNumber = this.patchCandidate.getLineNumber().get();
         assertThat(actualLineNumber).isEqualTo(3);
     }
+
+    /**
+     * getFilePathのテスト
+     */
+    @Test public void testForGetFilePath() {
+        String actualFilePath = this.patchCandidate.getFilePath();
+        String expectedFilePath = this.filePath;
+        assertThat(actualFilePath).isEqualTo(expectedFilePath);
+    }
+
+    /**
+     * getFqnのテスト
+     */
+    @Test public void testForGetFqn() {
+        String actualFqn = this.patchCandidate.getFqn();
+        String expectedFqn = this.fqn;
+        assertThat(actualFqn).isEqualTo(expectedFqn);
+    }
+
+    /**
+     * getCompilationUnitのテスト
+     */
+    @Test public void testForGetCompilationUnit() {
+        CompilationUnit actualCompilationUnit = this.patchCandidate.getCompilationUnit();
+        CompilationUnit expectedCompilationUnit = this.compilationUnit;
+        assertThat(actualCompilationUnit).isEqualTo(expectedCompilationUnit);
+    }
+
 }
