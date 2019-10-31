9528b0728c18962bab70c994db7f19383f8a2675
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-10-31 16:47:26 +0900

    Add FizzBuzz project for test

---
+++ src/test/resources/FizzBuzz01/src/main/java/FizzBuzz01/FizzBuzz.java
@@ -0,0 +1,19 @@
+package FizzBuzz01;
+
+public class FizzBuzz {
+    public String call(int i) {
+        if(i % 15 == 0)
+            return "FizzBuzz";
+        if(i % 5 == 0)
+            return "Buzz";
+        if(i % 3 == 0)
+            return "Fizz";
+        return String.valueOf(0); // bug here
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++){
+            System.out.println(new FizzBuzz().call(i));
+        }
+    }
+}
