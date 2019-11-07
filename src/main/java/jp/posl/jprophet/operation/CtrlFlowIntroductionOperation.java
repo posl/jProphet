f4bfa4ae555a995738a36c4fc03e3c3474eb075c
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-11-07 17:41:20 +0900

    Add inser ifstmt with break function

--- src/main/java/jp/posl/jprophet/operation/CtrlFlowIntroductionOperation.java
+++ src/main/java/jp/posl/jprophet/operation/CtrlFlowIntroductionOperation.java
@@ -3,12 +3,17 @@ package jp.posl.jprophet.operation;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.BreakStmt;
+import com.github.javaparser.ast.stmt.ForStmt;
 import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
 import com.github.javaparser.ast.stmt.Statement;
 
 import jp.posl.jprophet.RepairUnit;
@@ -22,24 +27,51 @@ public class CtrlFlowIntroductionOperation implements AstOperation{
     public List<RepairUnit> exec(RepairUnit repairUnit){
         Node targetNode = repairUnit.getTargetNode();
         if(!(targetNode instanceof Statement)) return new ArrayList<>();
+        if(targetNode instanceof BlockStmt) return new ArrayList<>();
         BlockStmt blockStmt; 
         try {
             blockStmt = targetNode.findParent(BlockStmt.class).orElseThrow();
         } catch (Exception e) {
             return new ArrayList<>();
         }
+
+        List<RepairUnit> candidates = new ArrayList<RepairUnit>();
+        this.insertIfStmtWithReturn(blockStmt, targetNode, repairUnit).map(candidates::add);
+        this.insertIfStmtWithBreak(blockStmt, targetNode, repairUnit).map(candidates::add);
+
+        return candidates;
+    }
+
+    public Optional<RepairUnit> insertIfStmtWithReturn(BlockStmt blockStmt, Node targetNode, RepairUnit repairUnit) {
         NodeList<Statement> statements = blockStmt.clone().getStatements();
         try {
-            statements.addBefore(new IfStmt(), (Statement)targetNode);
+            statements.addBefore(new IfStmt(null, new NameExpr("JPROPHET_ABST_HOLE") , new ReturnStmt(), null), (Statement) targetNode);
         } catch (Exception e) {
-            return new ArrayList<>();
+            return Optional.empty();
         }
 
         RepairUnit newCandidate = RepairUnit.deepCopy(repairUnit);
         newCandidate.getTargetNode().findParent(BlockStmt.class)
             .map(b -> b.setStatements(statements));
-        
-        List<RepairUnit> candidates = new ArrayList<RepairUnit>(Arrays.asList(newCandidate));
-        return candidates;
+    
+        return Optional.of(newCandidate);
+    }
+
+    public Optional<RepairUnit> insertIfStmtWithBreak(BlockStmt blockStmt, Node targetNode, RepairUnit repairUnit) throws IllegalArgumentException {
+        if(!targetNode.findParent(ForStmt.class).isPresent()) {
+            return Optional.empty();
+        }
+        NodeList<Statement> statements = blockStmt.clone().getStatements();
+        try {
+            statements.addBefore(new IfStmt(null, new NameExpr("JPROPHET_ABST_HOLE") , new BreakStmt((SimpleName) null), null), (Statement) targetNode);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+
+        RepairUnit newCandidate = RepairUnit.deepCopy(repairUnit);
+        newCandidate.getTargetNode().findParent(BlockStmt.class)
+            .map(b -> b.setStatements(statements));
+    
+        return Optional.of(newCandidate);
     }
 }
--- src/test/java/jp/posl/jprophet/operation/CtrlFlowIntroductionOperationTest.java
+++ src/test/java/jp/posl/jprophet/operation/CtrlFlowIntroductionOperationTest.java
@@ -10,8 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.github.javaparser.printer.PrettyPrinter;
-import com.github.javaparser.printer.PrettyPrinterConfiguration;
 
 public class CtrlFlowIntroductionOperationTest {
     /**
@@ -19,8 +17,8 @@ public class CtrlFlowIntroductionOperationTest {
      */
     @Test public void testForAddReturn(){
         final String targetSource = new StringBuilder().append("")
-            .append("public class A {\n")
-            .append("    private String fa = \"a\";\n")
+            .append("public class A {\n\n")
+            .append("    private String fa = \"a\";\n\n")
             .append("    private void ma() {\n")
             .append("        String la = \"b\";\n")
             .append("        fa = \"b\";\n")
@@ -31,10 +29,10 @@ public class CtrlFlowIntroductionOperationTest {
         List<String> expectedSources = new ArrayList<String>();
 
         expectedSources.add(new StringBuilder().append("")
-            .append("public class A {\n\n")
-            .append("    private String fa = \"a\";\n\n")
+            .append("public class A {\n\n") //toString(PrettyPrinter)での出力ではクラス宣言の先頭行の後に空行が入る仕様 
+            .append("    private String fa = \"a\";\n\n") //ここも
             .append("    private void ma() {\n")
-            .append("        if (false)\n")
+            .append("        if (JPROPHET_ABST_HOLE)\n")
             .append("            return;\n")
             .append("        String la = \"b\";\n")
             .append("        fa = \"b\";\n")
@@ -48,7 +46,7 @@ public class CtrlFlowIntroductionOperationTest {
             .append("    private String fa = \"a\";\n\n")
             .append("    private void ma() {\n")
             .append("        String la = \"b\";\n")
-            .append("        if (false)\n")
+            .append("        if (JPROPHET_ABST_HOLE)\n")
             .append("            return;\n")
             .append("        fa = \"b\";\n")
             .append("    }\n")
@@ -66,8 +64,47 @@ public class CtrlFlowIntroductionOperationTest {
                 .collect(Collectors.toList())
             );
         }
-        assertThat(candidateSources).containsOnlyElementsOf(expectedSources);
+        assertThat(candidateSources).containsAll(expectedSources);
         return;
     }
 
+    /**
+     * ループ構文内でbreakを行うifブロックが挿入されているかテスト
+     */
+    @Test public void testForAddBreakInLoop(){
+        final String targetSource = new StringBuilder().append("")
+            .append("public class A {\n\n")
+            .append("    private void ma() {\n")
+            .append("        for (int i = 0; i < 10; i++) {\n")
+            .append("            String la = \"b\";\n")
+            .append("        }\n")
+            .append("    }\n")
+            .append("}\n")
+            .toString();
+
+        String expectedSource = new StringBuilder().append("")
+            .append("public class A {\n\n")
+            .append("    private void ma() {\n")
+            .append("        for (int i = 0; i < 10; i++) {\n")
+            .append("            if (JPROPHET_ABST_HOLE)\n")
+            .append("                break;\n")
+            .append("            String la = \"b\";\n")
+            .append("        }\n")
+            .append("    }\n")
+            .append("}\n")
+            .toString();
+
+
+        List<RepairUnit> repairUnits = new AstGenerator().getAllRepairUnit(targetSource);
+        List<String> candidateSources = new ArrayList<String>();
+        for(RepairUnit repairUnit : repairUnits){
+            CtrlFlowIntroductionOperation vr = new CtrlFlowIntroductionOperation();
+            candidateSources.addAll(vr.exec(repairUnit).stream()
+                .map(ru -> ru.getCompilationUnit().toString()) 
+                .collect(Collectors.toList())
+            );
+        }
+        assertThat(candidateSources).contains(expectedSource);
+        return;
+    }
 }
