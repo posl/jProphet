bc5853ec93f7a50dae0be2f1b8694710a1bc0e20
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-11-12 20:11:52 +0900

    Add test for cond generator

---
+++ src/test/java/jp/posl/jprophet/operation/ConditionGeneratorTest.java
@@ -0,0 +1,51 @@
+package jp.posl.jprophet.operation;
+
+import java.util.List;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.stmt.IfStmt;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import jp.posl.jprophet.NodeUtility;
+
+
+public class ConditionGeneratorTest {
+    @Test public void test() {
+        String targetSource = new StringBuilder().append("")
+            .append("public class A {\n\n") 
+            .append("    boolean ba;\n\n")
+            .append("    private void ma() {\n")
+            .append("        if (JPROPHET_ABST_HOLE)\n")
+            .append("            return;\n")
+            .append("    }\n")
+            .append("}\n")
+            .toString();
+
+        IfStmt ifStmt = JavaParser.parse(targetSource).findFirst(IfStmt.class).get();
+        Expression abstHole = ifStmt.getCondition();
+
+        ConditionGenerator condGenerator = new ConditionGenerator();
+        Expression actualCondExpression = condGenerator.generateCondition(abstHole);
+
+        String expectedSource = new StringBuilder().append("")
+            .append("public class A {\n\n") 
+            .append("    boolean ba;\n\n")
+            .append("    private void ma() {\n")
+            .append("        if (ba == a)\n")
+            .append("            return;\n")
+            .append("    }\n")
+            .append("}\n")
+            .toString();
+        
+        IfStmt expectedIfStmt = JavaParser.parse(targetSource).findFirst(IfStmt.class).get();
+        Expression expectedCondExpression = ifStmt.getCondition();
+
+        assertThat(actualCondExpression).isEqualTo(expectedCondExpression);
+
+    }
+}
