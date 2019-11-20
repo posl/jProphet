3260c2e7a6ee792ae506d22144c6ccb051315948
Asada0815 <asada_mac@poslnoMacBook-puro.local>
2019-11-20 17:48:41 +0900

    make empty spotbugs integration test

---
+++ src/test/java/jp/posl/jprophet/SpotBugsIntegrationTest.java
@@ -0,0 +1,21 @@
+package jp.posl.jprophet;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+
+/**
+ * SpotBugsのワーニング解消機能の結合テスト
+ */
+public class SpotBugsIntegrationTest {
+
+
+    @Test
+    public void testForRoughConstantValue() {
+
+        
+    }
+
+
+
+}
