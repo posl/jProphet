29d8ad2e706ef6a0de8e2731e521531c0eedceac
Asada0815 <asada_mac@poslnoMacBook-puro.local>
2019-11-22 11:37:33 +0900

    add unit test class in test project

--- src/test/java/jp/posl/jprophet/SpotBugsIntegrationTest.java
+++ src/test/java/jp/posl/jprophet/SpotBugsIntegrationTest.java
@@ -35,9 +35,6 @@ public class SpotBugsIntegrationTest {
 
     @Before
     public void SetUpClasses() {
-
-
-
         this.operations = new ArrayList<AstOperation>(Arrays.asList(
             new CondRefinementOperation(),
             new CondIntroductionOperation(), 
@@ -57,12 +54,14 @@ public class SpotBugsIntegrationTest {
         
         File file = new File("result/result.csv");
         assertThat(file.exists()).isTrue();
+        
         try {
             FileUtils.deleteDirectory(new File("./result/"));
         } catch (IOException e) {
             System.err.println(e.getMessage());
             e.printStackTrace();
         }
+
     }
 
 
