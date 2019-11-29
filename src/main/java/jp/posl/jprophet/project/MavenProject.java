e1affdff99c328b171accb15defcef91cba099d3
Asada0815 <asada_mac@poslnoMacBook-puro.local>
2019-11-29 10:35:27 +0900

    create maven project class

---
+++ src/main/java/jp/posl/jprophet/project/MavenProject.java
@@ -0,0 +1,58 @@
+package jp.posl.jprophet.project;
+
+import java.util.List;
+
+public class MavenProject implements Project {
+
+    @Override
+    public List<FileLocator> getSrcFileLocators() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<FileLocator> getTestFileLocators() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getSrcFilePaths() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getTestFilePaths() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getSrcFileFqns() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getTestFileFqns() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getClassPaths() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getRootPath() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    
+
+}
