489829b12cdd8a386f9fbf3cec66fad29a7504c3
Yamate Kyosuke <yamate@poslnoMacBook-Pro.local>
2020-01-07 14:01:54 +0900

    Prepare to get Data

--- src/main/java/jp/posl/jprophet/JProphetMain.java
+++ src/main/java/jp/posl/jprophet/JProphetMain.java
@@ -76,13 +76,67 @@ public class JProphetMain {
             PatchedProjectGenerator patchedProjectGenerator, TestResultStore testResultStore, List<TestResultExporter> testResultExporters
             ) {
         // フォルトローカライゼーション
-        final List<Suspiciousness> suspiciousenesses = faultLocalization.exec();
+        final List<Suspiciousness> suspiciousnesses = faultLocalization.exec();
         
         // 各ASTに対して修正テンプレートを適用し抽象修正候補の生成
         final List<PatchCandidate> patchCandidates = patchCandidateGenerator.exec(config.getTargetProject(), operations);
         
         // 学習モデルやフォルトローカライゼーションのスコアによってソート
-        patchEvaluator.descendingSortBySuspiciousness(patchCandidates, suspiciousenesses);
+        patchEvaluator.descendingSortBySuspiciousness(patchCandidates, suspiciousnesses);
+
+        //パッチを順位をcsvに書き出し
+        final String resultDir = "./result/";
+        final String resultFilePath = "rank.csv";
+        final File resultDirFile = new File(resultDir);
+        if(!resultDirFile.exists()) {
+            resultDirFile.mkdir();
+        }
+        final File outputFile = new File(resultDir + resultFilePath);
+        if(outputFile.exists()) {
+            outputFile.delete();
+        }
+        final List<String> recodes = new ArrayList<String>();
+        final String field = "ID,filePath,line,operation,isSuccess";
+        recodes.add(field);
+        for (PatchCandidate patchCandidate : patchCandidates) {
+            final String patchLine = patchCandidate.getId() + "," + patchCandidate.getFilePath() + "," + patchCandidate.getLineNumber().get() + "," + patchCandidate.getAppliedOperation();
+            Project patchedProject = patchedProjectGenerator.applyPatch(patchCandidate);
+            final TestExecutorResult result = testExecutor.exec(new RepairConfiguration(config, patchedProject));
+            final String resultLine = "," + result.canEndRepair();
+            final String recode = patchLine + resultLine;
+            recodes.add(recode);
+        }
+        try {
+            FileUtils.write(outputFile, String.join("\n", recodes), "utf-8");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        //疑惑値のリストをcsvに
+        final String resultDir2 = "./result/";
+        final String resultFilePath2 = "suspiciousness.csv";
+        final File resultDirFile2 = new File(resultDir2);
+        if(!resultDirFile2.exists()) {
+            resultDirFile2.mkdir();
+        }
+        final File outputFile2 = new File(resultDir2 + resultFilePath2);
+        if(outputFile2.exists()) {
+            outputFile2.delete();
+        }
+        final List<String> recodes2 = new ArrayList<String>();
+        final String field2 = "FQN,Line,Value";
+        recodes.add(field2);
+        for (Suspiciousness susp : suspiciousnesses) {
+            final String recode = susp.getFQN() + "," + susp.getLineNumber() + "," + susp.getValue();
+            recodes2.add(recode);
+        }
+        try {
+            FileUtils.write(outputFile2, String.join("\n", recodes2), "utf-8");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
         
         // 修正パッチ候補ごとにテスト実行
         for(PatchCandidate patchCandidate: patchCandidates) {
