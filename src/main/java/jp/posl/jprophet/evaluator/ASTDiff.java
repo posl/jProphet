f48423086f2b087b050fb574f2c18ebdd98c9135
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-03-25 23:30:54 +0900

    Print AST diff list

--- src/main/java/jp/posl/jprophet/evaluator/ASTDiff.java
+++ src/main/java/jp/posl/jprophet/evaluator/ASTDiff.java
@@ -4,8 +4,28 @@ import java.util.List;
 
 import com.github.javaparser.ast.Node;
 
+import jp.posl.jprophet.NodeUtility;
+import difflib.Chunk;
+import difflib.Delta;
+import difflib.DiffUtils;
+import difflib.Patch;
+import difflib.Delta.TYPE;
+
 public class ASTDiff {
     public ASTDelta diff(Node original, Node revised) {
+        List<Node> originalNodes = NodeUtility.getAllDescendantNodes(original);
+        List<Node> revisedNodes = NodeUtility.getAllDescendantNodes(revised);
+
+	    Patch<Node> diff = DiffUtils.diff(originalNodes, revisedNodes);
+        List<Delta<Node>> deltas = diff.getDeltas();
+	    for (Delta<Node> delta : deltas) {
+            TYPE type = delta.getType();
+            System.out.println(type);
+            Chunk<Node> originalNode = delta.getOriginal();
+            Chunk<Node> fixedNode = delta.getRevised();
+            System.out.printf("del: position=%d, lines=%s%n", originalNode.getPosition(), originalNode.getLines());
+            System.out.printf("add: position=%d, lines=%s%n", fixedNode.getPosition(), fixedNode.getLines());
+        }
         return null;
     }
 
