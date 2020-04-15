feec67dcf0ce5ed8265ccef82920c62f4874def2
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-04-15 18:36:04 +0900

    Add new method to AstDiff

--- src/main/java/jp/posl/jprophet/evaluator/AstDiff.java
+++ src/main/java/jp/posl/jprophet/evaluator/AstDiff.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import com.github.javaparser.ast.Node;
 
 import jp.posl.jprophet.NodeUtility;
-import difflib.Chunk;
+import jp.posl.jprophet.evaluator.NodeWithDiffType.TYPE;
 import difflib.Delta;
 import difflib.DiffUtils;
 import difflib.Patch;
@@ -20,19 +20,36 @@ public class AstDiff {
      * @param revised 変更後のAST
      * @return 変更差分
      */
-    public List<AstDelta> diff(Node original, Node revised) {
+    public List<Delta<Node>> diff(Node original, Node revised) {
         final List<Node> originalNodes = NodeUtility.getAllDescendantNodes(original);
         final List<Node> revisedNodes = NodeUtility.getAllDescendantNodes(revised);
 
 	    final Patch<Node> diff = DiffUtils.diff(originalNodes, revisedNodes);
         final List<Delta<Node>> deltas = diff.getDeltas();
 
-        final List<AstDelta> astDeltas = deltas.stream().map((delta) -> {
-            final Chunk<Node> originalNode = delta.getOriginal();
-            final Chunk<Node> fixedNode = delta.getRevised();
-            return new AstDelta(originalNode.getLines(), fixedNode.getLines());
-        }).collect(Collectors.toList());
+        return deltas;
+    }
 
-        return astDeltas;
+    public NodeWithDiffType createRevisedAstWithDiffType(Node original, Node revised) {
+        List<Delta<Node>> deltas = diff(original, revised);
+        return createAstWithDiffType(revised, deltas);
+    }
+
+    private NodeWithDiffType createAstWithDiffType(Node targetNode, List<Delta<Node>> astDeltas) {
+        for(Delta<Node> astDelta: astDeltas) {
+            List<Node> diffNodes = astDelta.getOriginal().getLines();
+            diffNodes.addAll(astDelta.getRevised().getLines());
+            for(Node diffNode: diffNodes) {
+                if(diffNode.equals(targetNode) && diffNode.getRange().equals(targetNode.getRange())) {
+                    final List<NodeWithDiffType> childNodesWithDiffTypes = targetNode.getChildNodes().stream()
+                        .map(childNode -> createAstWithDiffType(childNode, astDeltas))
+                        .collect(Collectors.toList());
+                    final NodeWithDiffType nodeWithDiffType = new NodeWithDiffType(targetNode, TYPE.values()[astDelta.getType().ordinal()]);
+                    nodeWithDiffType.addChildNodes(childNodesWithDiffTypes);
+                    return nodeWithDiffType;
+                }
+            }
+        }
+        return new NodeWithDiffType(targetNode, TYPE.SAME);
     }
 }
---
+++ src/main/java/jp/posl/jprophet/evaluator/NodeWithDiffType.java
@@ -0,0 +1,48 @@
+package jp.posl.jprophet.evaluator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.javaparser.ast.Node;
+
+public class NodeWithDiffType {
+    final private Node node;
+    final private TYPE diffType;
+    final private List<NodeWithDiffType> childNodes = new ArrayList<NodeWithDiffType>();
+    
+    /**
+     * Specifies the type of the delta.
+     *
+     */
+    public enum TYPE {
+    	/** A change in the original. */
+        CHANGE, 
+        /** A delete from the original. */
+        DELETE, 
+        /** An insert into the original. */
+        INSERT,
+    	/** A same as the original. */
+        SAME
+    }
+
+    public NodeWithDiffType(Node node, TYPE diffType) {
+        this.node = node;
+        this.diffType = diffType;
+    }
+
+    public Node asNode() {
+        return this.node;
+    }
+
+    public List<NodeWithDiffType> getChildNodes() {
+        return this.childNodes;
+    }
+
+    public void addChildNodes(List<NodeWithDiffType> childNodes) {
+        childNodes.addAll(childNodes);
+    }
+
+    public TYPE getDiffType() {
+        return this.diffType;
+    }
+}
