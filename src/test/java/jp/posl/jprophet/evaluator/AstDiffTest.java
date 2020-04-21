84a837c69cb443185d14f5bfd1d54935e37f3b82
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-04-21 19:20:06 +0900

    Simplify the test

--- src/test/java/jp/posl/jprophet/evaluator/AstDiffTest.java
+++ src/test/java/jp/posl/jprophet/evaluator/AstDiffTest.java
@@ -3,10 +3,7 @@ package jp.posl.jprophet.evaluator;
 import static org.assertj.core.api.Assertions.assertThat;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.SimpleName;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.type.VoidType;
 
 import org.junit.Test;
 
@@ -15,6 +12,7 @@ import difflib.Delta;
 import java.util.List;
 
 import jp.posl.jprophet.NodeUtility;
+import jp.posl.jprophet.evaluator.NodeWithDiffType.TYPE;
 
 public class AstDiffTest {
     /**
@@ -46,20 +44,17 @@ public class AstDiffTest {
         assertThat(actualAstDelta.get(0).getOriginal().getLines().size()).isEqualTo(1);
         assertThat(actualAstDelta.get(0).getRevised().getLines().get(0)).isInstanceOf(SimpleName.class);
         assertThat(actualAstDelta.get(0).getOriginal().getLines().get(0)).isInstanceOf(SimpleName.class);
+        assertThat(actualAstDelta.get(0).getType()).isEqualTo(difflib.Delta.TYPE.CHANGE);
     }
 
     @Test public void testCreateRevisedAstWithDiffType() {
         final String originalSource = new StringBuilder().append("")
             .append("public class A {\n\n")
-            .append("    public void a() {\n")
-            .append("    }\n")
             .append("}\n")
             .toString();
 
         final String revisedSource = new StringBuilder().append("")
             .append("public class B {\n\n")
-            .append("    public void a() {\n")
-            .append("    }\n")
             .append("}\n")
             .toString();
 
@@ -69,18 +64,12 @@ public class AstDiffTest {
         final AstDiff astDiff = new AstDiff();
         final NodeWithDiffType nodeWithDiffType = astDiff.createRevisedAstWithDiffType(originalNodes.get(0), revisedNodes.get(0));
 
-        assertThat(nodeWithDiffType.getChildNodes().size()).isEqualTo(2);
+        assertThat(nodeWithDiffType.getChildNodes().size()).isEqualTo(1);
         assertThat(nodeWithDiffType.getNode()).isInstanceOf(ClassOrInterfaceDeclaration.class);
+        assertThat(nodeWithDiffType.getDiffType()).isEqualTo(TYPE.SAME);
 
         assertThat(nodeWithDiffType.getChildNodes().get(0).getChildNodes().size()).isEqualTo(0);
         assertThat(nodeWithDiffType.getChildNodes().get(0).getNode()).isInstanceOf(SimpleName.class);
-
-        assertThat(nodeWithDiffType.getChildNodes().get(1).getChildNodes().size()).isEqualTo(3);
-        assertThat(nodeWithDiffType.getChildNodes().get(1).getNode()).isInstanceOf(MethodDeclaration.class);
-
-        assertThat(nodeWithDiffType.getChildNodes().get(1).getChildNodes().get(0).getNode()).isInstanceOf(SimpleName.class);
-        assertThat(nodeWithDiffType.getChildNodes().get(1).getChildNodes().get(1).getNode()).isInstanceOf(VoidType.class);
-        assertThat(nodeWithDiffType.getChildNodes().get(1).getChildNodes().get(2).getNode()).isInstanceOf(BlockStmt.class);
+        assertThat(nodeWithDiffType.getChildNodes().get(0).getDiffType()).isEqualTo(TYPE.CHANGE);
     }
-        
 }
