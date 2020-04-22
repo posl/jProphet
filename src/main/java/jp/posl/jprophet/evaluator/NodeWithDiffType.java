f35db3a14907db6299d45279d2da8a8ff67ecf36
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-04-22 17:47:02 +0900

    Add javadoc to NodeWithDiffType

--- src/main/java/jp/posl/jprophet/evaluator/NodeWithDiffType.java
+++ src/main/java/jp/posl/jprophet/evaluator/NodeWithDiffType.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import com.github.javaparser.ast.Node;
 
+/**
+ * 差分の種類をNode型に付与したクラス．
+ */
 public class NodeWithDiffType {
     final private Node node;
     final private TYPE diffType;
@@ -12,7 +15,7 @@ public class NodeWithDiffType {
     
     /**
      * Specifies the type of the delta.
-     *
+     * DiffUtilsのTYPEを流用．SAMEを追加
      */
     public enum TYPE {
     	/** A change in the original. */
@@ -25,23 +28,40 @@ public class NodeWithDiffType {
         SAME
     }
 
+    /**
+     * @param node ノード
+     * @param diffType 差分の種類
+     */
     public NodeWithDiffType(Node node, TYPE diffType) {
         this.node = node;
         this.diffType = diffType;
     }
 
+    /**
+     * @return {@code Node}型のノード
+     */
     public Node getNode() {
         return this.node;
     }
 
+    /**
+     * @return 子ノードのリスト
+     */
     public List<NodeWithDiffType> getChildNodes() {
         return this.childNodes;
     }
 
+    /**
+     * 子ノードを追加する
+     * @param childNodes 追加する子ノードのリスト
+     */
     public void addChildNodes(List<NodeWithDiffType> childNodes) {
         this.childNodes.addAll(childNodes);
     }
 
+    /**
+     * @return 差分の種類
+     */
     public TYPE getDiffType() {
         return this.diffType;
     }
