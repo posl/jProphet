fc1ad6245c593287cdd0fbec66581d7268d929b1
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-04-23 18:24:39 +0900

    Add a function to judge the patch pattern.

--- src/main/java/jp/posl/jprophet/evaluator/PatchFeature.java
+++ src/main/java/jp/posl/jprophet/evaluator/PatchFeature.java
@@ -1,20 +1,41 @@
 package jp.posl.jprophet.evaluator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.Statement;
+
+import difflib.Delta;
+import jp.posl.jprophet.evaluator.NodeWithDiffType.TYPE;
 
 public class PatchFeature {
     void exec(Node original, Node revised) {
         final AstDiff astDiff = new AstDiff();
-        final List<AstDelta> astDeltas = astDiff.diff(original, revised);
-        final List<Integer> featureVec = new ArrayList<Integer>(List.of(0, 0, 0, 0, 0, 0));
-        astDeltas.stream().forEach((astDelta) -> {
-            final int deleteNodesSize = astDelta.getDeleteNodes().size();
-            final int addNodesSize = astDelta.getAddNodes().size();
-            final boolean inserted = addNodesSize != 0 && deleteNodesSize == 0;
-            final boolean changed  = addNodesSize != 0 && deleteNodesSize != 0;
-        });
+        final NodeWithDiffType nodeWithDiffType = astDiff.createRevisedAstWithDiffType(original, revised);
+        final Map<String, Integer> featureVec = new HashMap<String, Integer>();
+        featureVec.put("InsertControl", 0);
+        featureVec.put("InsertGuard",   0);
+        featureVec.put("ReplaceCond",   0);
+        featureVec.put("ReplaceStmt",   0);
+        featureVec.put("InsertStmt",    0);
+    }
+
+    void check(NodeWithDiffType nodeWithDiffType, Map<String, Integer> featureVec) {
+        if(nodeWithDiffType.getDiffType() == TYPE.INSERT) {
+            if(nodeWithDiffType.getNode() instanceof Statement) {
+                featureVec.replace("InsertStmt", featureVec.get("InsertStmt") + 1); 
+            }
+            // if(nodeWithDiffType.getNode() instanceof IfStmt) {
+            // }
+        }
+        if(nodeWithDiffType.getDiffType() == TYPE.CHANGE) {
+            if(nodeWithDiffType.getNode() instanceof Statement) {
+                featureVec.replace("ReplaceStmt", featureVec.get("ReplaceStmt") + 1); 
+            }
+        }
     }
 }
