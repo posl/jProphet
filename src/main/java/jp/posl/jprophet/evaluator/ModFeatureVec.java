e761aa25bbf90192d0645b24896cabe1f0fbfde7
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-05-01 20:53:05 +0900

    Add constructor to ModFeatureVec

--- src/main/java/jp/posl/jprophet/evaluator/ModFeatureVec.java
+++ src/main/java/jp/posl/jprophet/evaluator/ModFeatureVec.java
@@ -1,11 +1,27 @@
 package jp.posl.jprophet.evaluator;
 
 public class ModFeatureVec {
-    public int insertControl = 0;
-    public int insertGuard = 0;
-    public int replaceCond = 0;
-    public int replaceStmt = 0;
-    public int insertStmt = 0;
+    public int insertControl;
+    public int insertGuard;
+    public int replaceCond;
+    public int replaceStmt;
+    public int insertStmt;
+
+    public ModFeatureVec() {
+        insertControl = 0;
+        insertGuard = 0;
+        replaceCond = 0;
+        replaceStmt = 0;
+        insertStmt = 0;
+    };
+
+    public ModFeatureVec(int insertControl, int insertGuard, int replaceCond, int replaceStmt, int insertStmt) {
+        this.insertControl = insertControl;
+        this.insertGuard = insertGuard;
+        this.replaceCond = replaceCond;
+        this.replaceStmt = replaceStmt;
+        this.insertStmt = insertStmt;
+    };
 
     public void add(ModFeatureVec vec) {
         this.insertControl += vec.insertControl;
