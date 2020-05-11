c6b80c3482c178606dddfec8e0999e5992e74825
Yamate Kyosuke <yamate@poslnoMacBook-Pro.local>
2020-05-11 17:41:37 +0900

    Add MavenResourcesProject

---
+++ src/test/resources/MavenResourcesProject/src/main/java/MavenResourcesProject/ReadFile.java
@@ -0,0 +1,28 @@
+package MavenResourcesProject;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Properties;
+ 
+public class ReadFile {
+
+    public ReadFile() {
+        try {
+            InputStream is = this.getClass().getResourceAsStream("/test.txt");
+            Properties props = new Properties();
+            props.load(is);
+            is.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+ 
+    public static void main() {
+
+    }
+ 
+}
