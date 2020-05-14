911dface95ac26986b481d039d9b14cdca760db5
Yamate Kyosuke <yamate@poslnoMacBook-Pro.local>
2020-05-14 22:11:21 +0900

    Make MavenResourcesTest

--- src/test/java/jp/posl/jprophet/project/MavenResourcesTest.java
+++ src/test/java/jp/posl/jprophet/project/MavenResourcesTest.java
@@ -1,80 +1,59 @@
 package jp.posl.jprophet.project;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
+import org.junit.Before;
 import org.junit.Test;
 
-import jp.posl.jprophet.JProphetMain;
-import jp.posl.jprophet.PatchCandidateGenerator;
-import jp.posl.jprophet.PatchedProjectGenerator;
+import jp.posl.jprophet.ProjectBuilder;
 import jp.posl.jprophet.RepairConfiguration;
-import jp.posl.jprophet.evaluator.PatchEvaluator;
-import jp.posl.jprophet.fl.FaultLocalization;
-import jp.posl.jprophet.fl.spectrumbased.SpectrumBasedFaultLocalization;
-import jp.posl.jprophet.fl.spectrumbased.strategy.*;
-import jp.posl.jprophet.operation.*;
-import jp.posl.jprophet.test.executor.TestExecutor;
-import jp.posl.jprophet.test.executor.UnitTestExecutor;
-import jp.posl.jprophet.test.exporter.CSVTestResultExporter;
-import jp.posl.jprophet.test.exporter.PatchDiffExporter;
-import jp.posl.jprophet.test.exporter.TestResultExporter;
-import jp.posl.jprophet.test.result.TestResultStore;
+import jp.posl.jprophet.fl.spectrumbased.coverage.CoverageCollector;
+import jp.posl.jprophet.fl.spectrumbased.coverage.TestResults;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
-
-import java.util.Arrays;
-import java.util.List;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class MavenResourcesTest {
+    TestResults testResults;
+    boolean isSuccess;
 
-    
-    @Test public void testForResources(){
+    @Before public void setUp(){
         final String buildDir = "./tmp/"; 
         final String resultDir = "./result/"; 
         String projectPath = "src/test/resources/MavenResourcesProject";
 
-
         final Project                  project                  = new MavenProject(projectPath);
         final RepairConfiguration      config                   = new RepairConfiguration(buildDir, resultDir, project);
-        final Coefficient              coefficient              = new Jaccard();
-        final FaultLocalization        faultLocalization        = new SpectrumBasedFaultLocalization(config, coefficient);
-        final PatchCandidateGenerator  patchCandidateGenerator  = new PatchCandidateGenerator();
-        final PatchEvaluator           patchEvaluator           = new PatchEvaluator();
-        final TestExecutor             testExecutor             = new UnitTestExecutor();
-        final PatchedProjectGenerator  patchedProjectGenerator  = new PatchedProjectGenerator(config);
-        final TestResultStore          testResultStore          = new TestResultStore();
 
-        final List<TestResultExporter> testResultExporters = new ArrayList<TestResultExporter>(Arrays.asList(
-            new CSVTestResultExporter(resultDir),
-            new PatchDiffExporter(resultDir)
-        ));
+        ProjectBuilder builder = new ProjectBuilder();
+        this.isSuccess = builder.build(config);
 
-        final List<AstOperation> operations = new ArrayList<AstOperation>(Arrays.asList(
-            new CondRefinementOperation(),
-            new CondIntroductionOperation(), 
-            new CtrlFlowIntroductionOperation(), 
-            new InsertInitOperation(), 
-            new VariableReplacementOperation(),
-            new CopyReplaceOperation()
-        ));
-
-
-        final JProphetMain jprophet = new JProphetMain();
-        final boolean isRepairSuccess = jprophet.run(config, faultLocalization, patchCandidateGenerator, operations, patchEvaluator, testExecutor, patchedProjectGenerator, testResultStore, testResultExporters);
+        CoverageCollector coverageCollector = new CoverageCollector(config.getBuildPath());
+        try {
+            this.testResults = coverageCollector.exec(config.getTargetProject().getSrcFileFqns(), config.getTargetProject().getTestFileFqns());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(-1);
+        }
         try {
             FileUtils.deleteDirectory(new File(buildDir));
-            if(!isRepairSuccess){
-                FileUtils.deleteDirectory(new File(config.getFixedProjectDirPath() + FilenameUtils.getBaseName(project.getRootPath()))); //失敗した場合でもログは残す
-            }
         } catch (IOException e) {
             System.err.println(e.getMessage());
             e.printStackTrace();
         }
     }
+
+    /**
+     * src/test/resources/のファイルを参照するようなテストに対応しているかのテスト
+     */
+    @Test public void testForResources() {
+        assertThat(this.isSuccess); //ビルドができたか確認
+        assertThat(!testResults.getTestResults().get(0).wasFailed()); //対象プロジェクトのテストの実行結果が正しいか
+        assertThat(testResults.getTestResults().get(0).getCoverages().size()).isEqualTo(1); //対象プロジェクトのテストカバレッジが取れているか
+    }
+
+
     
     
     
