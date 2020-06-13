d06bf6724ee3df1b976f574100a2ac86cbfca848
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-06-14 00:36:28 +0900

    Create a part of FeatureExtractor

--- src/main/java/jp/posl/jprophet/evaluator/FeatureExtractor.java
+++ src/main/java/jp/posl/jprophet/evaluator/FeatureExtractor.java
@@ -17,9 +17,42 @@ public class FeatureExtractor {
 
         final ModFeatureExtractor modFeatureExtractor = new ModFeatureExtractor();
         final Map<ProgramChank,ModFeature> modFeatureMap = modFeatureExtractor.extract(nodeWithDiffType, chanks);
-        final StatementFeatureExtractor stmtFeatureExtractorForOriginal = new StatementFeatureExtractor(originalRoot);
-        final StatementFeatureExtractor stmtFeatureExtractorForFixed = new StatementFeatureExtractor(fixedRoot);
+        final StatementFeatureExtractor stmtFeatureExtractor = new StatementFeatureExtractor(originalRoot);
+        modFeatureMap.keySet().stream()
+            .forEach(chank -> {
+                final ModFeature modFeature = modFeatureMap.get(chank);
+                for(int line = chank.getBegin(); line <= chank.getEnd(); line++) {
+                    final StatementFeature stmtFeature = stmtFeatureExtractor.extract(line);
+                    this.toIndex(StmtPos.TARGET, stmtFeature, modFeature);
+                }
+                final int prev3StmtsBegin =  chank.getBegin() - 4;
+                final int prev3StmtsEnd =  chank.getBegin() - 1;
+                for(int line = prev3StmtsBegin; line <= prev3StmtsEnd; line++) {
+                    if(line < 1) continue;
+                    final StatementFeature stmtFeature = stmtFeatureExtractor.extract(line);
+                    this.toIndex(StmtPos.PREV, stmtFeature, modFeature);
+                }
+                final int next3StmtsBegin =  chank.getEnd() + 1;
+                final int next3StmtsEnd =  chank.getEnd() + 4;
+                for(int line = next3StmtsBegin; line <= next3StmtsEnd; line++) {
+                    if(line > originalRoot.getEnd().get().line) {
+                        break;
+                    }
+                    final StatementFeature stmtFeature = stmtFeatureExtractor.extract(line);
+                    this.toIndex(StmtPos.NEXT, stmtFeature, modFeature);
+                }
+            });
 
         return new FeatureVector();
+    }
+
+    enum StmtPos {
+        TARGET,
+        PREV,
+        NEXT
+    }
+
+    private int toIndex(StmtPos stmtPos, StatementFeature stmtFeature, ModFeature modFeature) {
+        return 0;
     }
 }
