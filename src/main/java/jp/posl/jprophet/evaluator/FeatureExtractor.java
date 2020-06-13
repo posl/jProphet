57b1d75f1a2e7c1fd05bc3e2f1a511127b10507e
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-06-13 20:38:20 +0900

    Add new method for patch candidate

--- src/main/java/jp/posl/jprophet/evaluator/FeatureExtractor.java
+++ src/main/java/jp/posl/jprophet/evaluator/FeatureExtractor.java
@@ -1,7 +1,25 @@
 package jp.posl.jprophet.evaluator;
 
+import java.util.List;
+import java.util.Map;
+
+import com.github.javaparser.ast.Node;
+
+import jp.posl.jprophet.patch.DefaultPatchCandidate;
+
 public class FeatureExtractor {
-    public FeatureVector extract() {
+    public FeatureVector extract(DefaultPatchCandidate patch) {
+        Node originalRoot = patch.getOriginalCompilationUnit().findRootNode();
+        Node fixedRoot = patch.getCompilationUnit().findRootNode();
+        final AstDiff diff = new AstDiff();
+        final NodeWithDiffType nodeWithDiffType = diff.createRevisedAstWithDiffType(originalRoot, fixedRoot);
+        final List<ProgramChank> chanks = nodeWithDiffType.identifyModifiedProgramChanks();
+
+        final ModFeatureExtractor modFeatureExtractor = new ModFeatureExtractor();
+        final Map<ProgramChank,ModFeature> modFeatureMap = modFeatureExtractor.extract(nodeWithDiffType, chanks);
+        final StatementFeatureExtractor stmtFeatureExtractorForOriginal = new StatementFeatureExtractor(originalRoot);
+        final StatementFeatureExtractor stmtFeatureExtractorForFixed = new StatementFeatureExtractor(fixedRoot);
+
         return new FeatureVector();
     }
 }
