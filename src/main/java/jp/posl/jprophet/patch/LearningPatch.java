9f60d2d23fd96fa89b5326ff73562da76e5f4188
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-08-15 00:29:28 +0900

    Add javadoc and rename file

--- src/main/java/jp/posl/jprophet/patch/LearningPatch.java
+++ src/main/java/jp/posl/jprophet/patch/LearningPatch.java
@@ -6,10 +6,19 @@ import com.github.javaparser.ast.CompilationUnit;
 
 import jp.posl.jprophet.NodeUtility;
 
+/**
+ * 学習用のパッチを表現するクラス
+ */
 public class LearningPatch implements Patch {
     final private CompilationUnit fixedCompilationUnit;
     final private CompilationUnit originalCompilationUnit;
 
+    /**
+     * 修正前後のソースコードを元にパッチクラスを作成
+     * @param originalSourceCode 修正前コード
+     * @param fixedSourceCode 修正後コード
+     * @throws ParseProblemException
+     */
     public LearningPatch(String originalSourceCode, String fixedSourceCode) throws ParseProblemException {
         final CompilationUnit originalCu = JavaParser.parse(originalSourceCode);
         final CompilationUnit fixedCu = JavaParser.parse(fixedSourceCode);
@@ -24,15 +33,26 @@ public class LearningPatch implements Patch {
         this.fixedCompilationUnit = JavaParser.parse(fixedCu.toString());
     }
 
+    /**
+     * 修正前後のCompilationUnitを元にパッチクラスを作成
+     * @param originalCompilationUnit 修正前のCompilationUnit
+     * @param fixedCompilationUnit 修正後のCompilationUnit
+     */
     public LearningPatch(CompilationUnit originalCompilationUnit, CompilationUnit fixedCompilationUnit) {
         this.originalCompilationUnit = JavaParser.parse(originalCompilationUnit.toString());
         this.fixedCompilationUnit = JavaParser.parse(fixedCompilationUnit.toString());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public CompilationUnit getFixedCompilationUnit() {
         return this.fixedCompilationUnit;        
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public CompilationUnit getOriginalCompilationUnit() {
         return this.originalCompilationUnit;        
     }
