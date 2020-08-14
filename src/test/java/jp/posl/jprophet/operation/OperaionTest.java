ddb7196973e768278466b1ce1c8fe556f6f49ba8
Asada0815 <asada_mac@poslnoMacBook-puro.local>
2020-08-14 15:02:06 +0900

    create OperationTest

---
+++ src/test/java/jp/posl/jprophet/operation/OperaionTest.java
@@ -0,0 +1,27 @@
+package jp.posl.jprophet.operation;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.github.javaparser.ast.Node;
+
+import jp.posl.jprophet.NodeUtility;
+import jp.posl.jprophet.patch.DiffWithType;
+
+public class OperaionTest {
+    
+    public void test(String targetSource, List<String> expectedSources, AstOperation operation) {
+        final List<Node> nodes = NodeUtility.getAllNodesFromCode(targetSource);
+        final List<String> actualSources = new ArrayList<String>();
+        for(Node node : nodes){
+            final List<DiffWithType> results = operation.exec(node);
+            actualSources.addAll(results.stream().map(result -> result.getTargetNodeAfterFix().toString()).collect(Collectors.toList()));
+        }
+        assertThat(actualSources).containsOnlyElementsOf(expectedSources);
+        return;
+    }
+    
+}
