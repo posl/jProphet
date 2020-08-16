9dc757b8bda5f9bf1e851afbce8d8f78f8c55554
Yamate Kyosuke <yamate@poslnoMacBook-Pro-2.local>
2020-08-16 21:15:20 +0900

    Fix CondIntroductionTest

--- src/test/java/jp/posl/jprophet/operation/CondIntroductionOperationTest.java
+++ src/test/java/jp/posl/jprophet/operation/CondIntroductionOperationTest.java
@@ -15,7 +15,7 @@ public class CondIntroductionOperationTest {
     /**
      * returnをするifブロックが挿入されているかテスト
      */
-    /*
+    
     @Test public void test(){
         final String targetSource = new StringBuilder().append("")
             .append("public class A {\n\n")
@@ -30,41 +30,44 @@ public class CondIntroductionOperationTest {
         final List<String> expectedSources = new ArrayList<String>();
 
         expectedSources.add(new StringBuilder().append("")
-            .append("public class A {\n\n") //toString(PrettyPrinter)での出力ではクラス宣言の先頭行の後に空行が入る仕様 
-            .append("    private String fa = \"a\";\n\n") //ここも
-            .append("    private void ma() {\n")
-            .append("        if (true)\n")
-            .append("            la = \"b\";\n")
-            .append("        fa = \"b\";\n")
-            .append("    }\n")
-            .append("}\n")
+            .append("if (fa == null)\n")
+            .append("    la = \"b\";")
             .toString()
         );
 
         expectedSources.add(new StringBuilder().append("")
-            .append("public class A {\n\n")
-            .append("    private String fa = \"a\";\n\n")
-            .append("    private void ma() {\n")
-            .append("        la = \"b\";\n")
-            .append("        if (true)\n")
-            .append("            fa = \"b\";\n")
-            .append("    }\n")
-            .append("}\n")
+            .append("if (fa != null)\n")
+            .append("    la = \"b\";")
             .toString()
         );
 
-        final List<Node> nodes = NodeUtility.getAllNodesFromCode(targetSource);
-        final List<String> candidateSources = new ArrayList<String>();
-        for(Node node : nodes){
-            final CondIntroductionOperation cio = new CondIntroductionOperation();
-            candidateSources.addAll(cio.exec(node).stream()
-                .map(ru -> ru.findCompilationUnit().get().toString()) 
-                .collect(Collectors.toList())
-            );
-        }
+        expectedSources.add(new StringBuilder().append("")
+            .append("if (true)\n")
+            .append("    la = \"b\";")
+            .toString()
+        );
 
-        assertThat(candidateSources).containsAll(expectedSources);
+        expectedSources.add(new StringBuilder().append("")
+            .append("if (fa == null)\n")
+            .append("    fa = \"b\";")
+            .toString()
+        );
+
+        expectedSources.add(new StringBuilder().append("")
+            .append("if (fa != null)\n")
+            .append("    fa = \"b\";")
+            .toString()
+        );
+        
+        expectedSources.add(new StringBuilder().append("")
+            .append("if (true)\n")
+            .append("    fa = \"b\";")
+            .toString()
+        );
+
+        new OperaionTest().test(targetSource, expectedSources, new CondIntroductionOperation());
+        
         return;
     }
-    */
+    
 }
