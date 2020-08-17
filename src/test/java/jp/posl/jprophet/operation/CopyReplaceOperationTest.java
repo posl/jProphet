4299a2268be514bcb60b26cc34abe00c53daf172
Asada0815 <asada_mac@poslnoMacBook-puro.local>
2020-08-18 02:48:48 +0900

    add cpoy-replace-op test code

--- src/test/java/jp/posl/jprophet/operation/CopyReplaceOperationTest.java
+++ src/test/java/jp/posl/jprophet/operation/CopyReplaceOperationTest.java
@@ -2,18 +2,12 @@ package jp.posl.jprophet.operation;
 
 import org.junit.Test;
 
-import jp.posl.jprophet.NodeUtility;
-import jp.posl.jprophet.patch.DiffWithType;
 
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
-import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
 
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CopyReplaceOperationTest{
     
@@ -39,7 +33,6 @@ public class CopyReplaceOperationTest{
             .append("}\n")
             .toString();
 
-
         final List<String> expectedSources = new ArrayList<String>();
         expectedSources.add("this.mb(this.fa, \"fuga\");");
         expectedSources.add("this.mb(\"hoge\", this.fa);");
@@ -52,100 +45,37 @@ public class CopyReplaceOperationTest{
         expectedSources.add("this.mb(pb, \"fuga\");");
         expectedSources.add("this.mb(\"hoge\", pb);");
 
-        final List<Node> nodes = NodeUtility.getAllNodesFromCode(targetSource);
-        final List<String> actualSources = new ArrayList<String>();
-        for(Node node : nodes){
-            final CopyReplaceOperation cr = new CopyReplaceOperation();
-            final List<DiffWithType> results = cr.exec(node);
-            for (DiffWithType result : results){
-                System.out.println(result.getTargetNodeAfterFix().toString());
-                actualSources.add(result.getTargetNodeAfterFix().toString());
-            }
-        }
-        assertThat(actualSources).containsOnlyElementsOf(expectedSources);
-        return;
+        OperationTest operationTest = new OperationTest();
+        operationTest.test(targetSource, expectedSources, new CopyReplaceOperation());
     }
 
     /**
      * if文が含まれる場合のテスト
      */
-    /*
     @Test public void testForIfStatementCopy(){
 
-        final String beforeTargetStatement = new StringBuilder().append("")
+        final String targetSource = new StringBuilder().append("")
             .append("public class A {\n")
             .append("    private String fa = \"a\";\n")
             .append("    private void ma(String pa) {\n")
             .append("        String la = \"a\";\n")
             .append("        String lb = \"b\";\n")
-            .toString();
-        
-        final String statementToBeCopied = 
-            "        la = \"hoge\";\n";
-
-        final String targetStatement = new StringBuilder().append("")
+            .append("        la = \"hoge\";\n")
             .append("        if (true) {\n")
             .append("            lb = \"huga\";\n")
             .append("        }\n")
-            .toString();
-
-        final String afterTargetStatement = new StringBuilder().append("")
             .append("    }\n")
             .append("}\n")
             .toString();
 
-        final String targetSource = new StringBuilder().append("")
-            .append(beforeTargetStatement)
-            .append(statementToBeCopied)
-            .append(targetStatement)
-            .append(afterTargetStatement)
-            .toString();
+        List<String> expectedSources = new ArrayList<String>();
+        expectedSources.add("la = la;");
+        expectedSources.add("la = lb;");
+        expectedSources.add("la = this.fa;");
+        expectedSources.add("la = pa;");
 
-
-        List<String> expectedTargetSources = new ArrayList<String>();
-        expectedTargetSources.add("        la = la;\n");
-        expectedTargetSources.add("        la = lb;\n");
-        expectedTargetSources.add("        la = this.fa;\n");
-        expectedTargetSources.add("        la = pa;\n");
-
-        List<String> expectedSources = expectedTargetSources.stream()
-            .map(str -> {
-                return new StringBuilder().append("")
-                    .append(beforeTargetStatement)
-                    .append(statementToBeCopied)
-                    .append(str)
-                    .append(targetStatement)
-                    .append(afterTargetStatement)
-                    .toString();
-            })
-            .collect(Collectors.toList());
-
-        expectedSources.addAll(expectedTargetSources.stream()
-            .map(str -> {
-                return new StringBuilder().append("")
-                    .append(beforeTargetStatement)
-                    .append(statementToBeCopied)
-                    .append("        if (true) {\n")
-                    .append("    " + str)
-                    .append("            lb = \"huga\";\n")
-                    .append("        }\n")
-                    .append(afterTargetStatement)
-                    .toString();
-            })
-            .collect(Collectors.toList()));
-
-        List<Node> nodes = NodeUtility.getAllNodesFromCode(targetSource);
-        List<String> candidateSources = new ArrayList<String>();
-        for(Node node : nodes){
-            CopyReplaceOperation cr = new CopyReplaceOperation();
-            List<CompilationUnit> cUnits = cr.exec(node);
-            for (CompilationUnit cUnit : cUnits){
-                LexicalPreservingPrinter.setup(cUnit);
-                candidateSources.add(LexicalPreservingPrinter.print(cUnit));
-            }
-        }
-        assertThat(candidateSources).containsOnlyElementsOf(expectedSources);
-        return;
+        OperationTest operationTest = new OperationTest();
+        operationTest.test(targetSource, expectedSources, new CopyReplaceOperation());
     }
 
     /**
@@ -216,7 +146,6 @@ public class CopyReplaceOperationTest{
     /**
      * 少し複雑なコードで動作するかどうかテスト
      */
-    /*
     @Test public void testForRun(){
 
         final String targetSource = new StringBuilder().append("")
@@ -241,24 +170,13 @@ public class CopyReplaceOperationTest{
             .append("}\n")
             .toString();
 
-        List<Node> nodes = NodeUtility.getAllNodesFromCode(targetSource);
-        List<String> candidateSources = new ArrayList<String>();
-        for(Node node : nodes){
-            CopyReplaceOperation cr = new CopyReplaceOperation();
-            List<CompilationUnit> cUnits = cr.exec(node);
-            for (CompilationUnit cUnit : cUnits){
-                LexicalPreservingPrinter.setup(cUnit);
-                String str = LexicalPreservingPrinter.print(cUnit);
-                candidateSources.add(str);
-            }
-        }
-        return;
+        OperationTest operationTest = new OperationTest();
+        //operationTest.test(targetSource, new ArrayList<String>(), new CopyReplaceOperation());
     }
 
     /**
      * switch文がある時にエラーが起きないかテスト
      */
-    /*
     @Test public void testForSwitch(){
 
         final String targetSource = new StringBuilder().append("")
@@ -286,18 +204,7 @@ public class CopyReplaceOperationTest{
             .append("}\n")
             .toString();
 
-        List<Node> nodes = NodeUtility.getAllNodesFromCode(targetSource);
-        List<String> candidateSources = new ArrayList<String>();
-        for(Node node : nodes){
-            CopyReplaceOperation cr = new CopyReplaceOperation();
-            List<CompilationUnit> cUnits = cr.exec(node);
-            for (CompilationUnit cUnit : cUnits){
-                LexicalPreservingPrinter.setup(cUnit);
-                String str = LexicalPreservingPrinter.print(cUnit);
-                candidateSources.add(str);
-            }
-        }
-        return;
+        OperationTest operationTest = new OperationTest();
+        //operationTest.test(targetSource, new ArrayList<String>(), new CopyReplaceOperation());
     }
-    */
 }
